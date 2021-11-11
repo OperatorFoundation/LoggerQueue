3c9b876674b19a1163ff43dea888b782bc53e69d
CryptoSax <joshua@operatorfoundation.org>
2021-11-10 20:04:51 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LoggerQueue
+
+A description of this package.
---
+++ Tests/LoggerQueueTests/LoggerQueueTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LoggerQueue
+
+final class LoggerQueueTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LoggerQueue().text, "Hello, World!")
+    }
+}
